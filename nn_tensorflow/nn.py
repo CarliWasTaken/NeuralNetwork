2fc3bfc2a47c4cc5ee051e78fe374a86acf24111
Jonas <seijod17@htl-kaindorf.at>
2022-02-12 13:04:09 +0100

    Working on tf nn

--- nn_tensorflow/nn.py
+++ nn_tensorflow/nn.py
@@ -4,44 +4,62 @@ import matplotlib.pyplot as plt
 from tensorflow import keras
 import os
 
-def load_images(folder):
+def load_images(path):
     images = []
     labels = []
-    for filename in os.listdir(folder):
-        label = (float(filename.split('_')[2].replace('.jpg', ''))+1)*0.5
-        labels.append(label)
-        img = cv2.imread(os.path.join(folder,filename), 0)
-        if img is not None:
-            images.append(img)
+    print("=============================")
+    print("Loading images from: ", path)
+    for root, dirs, files in os.walk(path):
+        for dir in dirs:
+            for file in os.listdir(path + dir):
+                full_path = os.path.join(path, dir, file)
+                
+                img = cv2.imread(full_path, 0)
+                img = img[int(img.shape[0]/3):,:]
+                img = cv2.Canny(img, 100, 200)
+                img = cv2.resize(img, (0,0), fx=0.25, fy=0.25)
+
+                label = float(full_path.split('_')[1].replace('.jpg', ''))
+                label = (label+1)*0.5
+                
+                images.append(img.flatten())
+                labels.append(label)
+                
+    print("Done loading images:")
+    print("Number of images: ", len(images))
+    print("=============================")
     return (images, labels)
 
 
-images, labels = load_images('data')
+images, labels = load_images('data\\2\\')
 
-#test_images = np.asarray(images[-int(len(images)*0.1):])
-#test_labels = np.asarray(labels[-int(len(labels)*0.1):])
-
-train_images = np.asarray(images)
-train_labels = np.asarray(labels)
+images = np.asarray(images)
+labels = np.asarray(labels)
 
 
 model = keras.Sequential([
-    keras.layers.Input(shape=(48, 64)),
-    keras.layers.Dense(32, activation='relu'),
-    keras.layers.Dense(32, activation='relu'),
-    keras.layers.Dense(32, activation='relu'),
-    keras.layers.Dense(32, activation='relu'),
+    keras.layers.Input(shape=(3200,)),
+    keras.layers.Dense(2500, activation='sigmoid'),
     keras.layers.Dense(1, activation='sigmoid')
 ])
 model.compile(optimizer='adam', loss='mse')
 
+model.summary()
+
 model.fit(
-    train_images, 
-    train_labels, 
-    epochs=10,
-    batch_size=1,
+    images, 
+    labels, 
+    epochs=5,
+    batch_size=10,
     validation_split=0.1,
     shuffle=True
 )
 
-model.save('model.h5')
+for i, img in enumerate(images):
+    
+    img = np.expand_dims(img, axis=0)
+    print(f"Should be: {labels[i]}")
+    print(f"Prediction: {model(img)}")
+    img = img.reshape(40, 80)
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
